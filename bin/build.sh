b02c0c63e2865b110eb8e0fa3b259a67c71991ee
Will Pimblett <will@wjdp.uk>
2022-07-09 18:30:28 +0100

    Add poetry install to build script
    CF Pages doesn't have this by default

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
+
+# Install poetry
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+
+# Install dependencies
 poetry install
 
+# Build the project
 time poetry run python nthp load
 time poetry run python nthp stats
 time poetry run python nthp dump
