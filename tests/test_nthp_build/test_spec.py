6e9eab3cb837e230dd2287073b675180c3abdc86
Will Pimblett <will@wjdp.uk>
2023-05-20 20:40:21 +0100

    Fix linting errors in tests

--- tests/test_nthp_build/test_spec.py
+++ tests/test_nthp_build/test_spec.py
@@ -4,5 +4,7 @@ from nthp_build import spec
 def test_spec_generates():
     """Basic sanity checks"""
     assert spec.SPEC["openapi"] == "3.1.0"
-    assert len(spec.SPEC["paths"]) > 2
-    assert len(spec.SPEC["components"]["schemas"]) > 2
+    expected_number_of_paths = 2
+    assert len(spec.SPEC["paths"]) > expected_number_of_paths
+    expected_number_of_schema = 2
+    assert len(spec.SPEC["components"]["schemas"]) > expected_number_of_schema
--- tests/test_smugmugger/test_schema.py
+++ tests/test_smugmugger/test_schema.py
@@ -1,3 +1,5 @@
+from http import HTTPStatus
+
 from smugmugger import schema
 
 from .fixtures import albumimages
@@ -6,16 +8,16 @@ from .fixtures import albumimages
 class TestSmugMugResponse:
     def test_ok(self):
         response = schema.SmugMugResponse(**albumimages.three_images_many_pages)
-        assert response.Code == 200
+        assert response.Code == HTTPStatus.OK
         assert response.Message == "Ok"
 
     def test_pages(self):
         response = schema.SmugMugResponse(**albumimages.three_images_many_pages)
         pages = response.Response.Pages
-        assert pages.Total == 379
+        assert pages.Total == 379  # noqa: PLR2004
         assert pages.Start == 1
-        assert pages.Count == 3
-        assert pages.RequestedCount == 3
+        assert pages.Count == 3  # noqa: PLR2004
+        assert pages.RequestedCount == 3  # noqa: PLR2004
         assert pages.FirstPage == "/api/v2/album/dvVPZh!images?count=3&start=1"
         assert pages.LastPage == "/api/v2/album/dvVPZh!images?count=3&start=379"
         assert pages.NextPage == "/api/v2/album/dvVPZh!images?count=3&start=4"
